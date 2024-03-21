87cca4ca9ebeca9f2b8b75a2ce209c897cdc1bfd
Xan <octoxan@users.noreply.github.com>
2024-03-20 21:08:26 -0400

    disable plugin update checking if acorns or acf-composer version is too low

--- embold-tailwind-blocks.php
+++ embold-tailwind-blocks.php
@@ -4,7 +4,7 @@
  * Plugin Name:        emBold Tailwind Blocks
  * Plugin URI:         https://embold.com
  * Description:        A collection of Tailwind Blocks, written with ACF Composer and Blade Templates. REQUIRES ACF to be enabled!
- * Version:            0.12.0
+ * Version:            1.0.0
  * Author:             emBold
  * Author URI:         https://embold.com/
  * Primary Branch:     master
@@ -29,17 +29,71 @@ if (!is_plugin_active('advanced-custom-fields/acf.php') && !is_plugin_active('ad
     return;
 }
 
+global $allow_updates;
+$allow_updates = true;
+
+function check_theme_acorn_version() {
+    global $allow_updates;
+
+    // Get the active theme directory
+    $theme_dir = get_template_directory();
+
+    // Check if the theme directory exists
+    if (is_dir($theme_dir)) {
+        // Get the path to the composer.json file of the active theme
+        $composer_json_path = $theme_dir.'/composer.json';
+
+        // Check if composer.json file exists
+        if (file_exists($composer_json_path)) {
+            // Read the composer.json file
+            $composer_json = file_get_contents($composer_json_path);
+
+            // Decode the JSON data
+            $composer_data = json_decode($composer_json, true);
+
+            // Check if the composer.json data contains 'require' key
+            if (isset($composer_data['require'])) {
+                // Check if Acorn is listed in the 'require' section
+                if (isset($composer_data['require']['roots/acorn'])) {
+                    $acorn_version = str_replace('^', '', $composer_data['require']['roots/acorn']);
+
+                    // Check if the Acorn version is v3 or v4
+                    if (version_compare($acorn_version, '4.0.0', '<')) {
+                        $allow_updates = false;
+                    }
+                }
+
+                if (isset($composer_data['require']['log1x/acf-composer'])) {
+                    $acf_composer_version = str_replace('^', '', $composer_data['require']['log1x/acf-composer']);
+
+                    // Check if the Acorn version is v3 or v4
+                    if (version_compare($acf_composer_version, '2.9.0', '<')) {
+                        $allow_updates = false;
+                    }
+                }
+            }
+        }
+    }
+}
+
+add_action('plugins_loaded', 'check_theme_acorn_version');
+
 require 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
-$embold_update_checker = PucFactory::buildUpdateChecker(
-	'https://github.com/emboldagency/embold-tailwind-blocks/',
-	__FILE__,
-	'embold-tailwind-blocks'
-);
+add_action('plugins_loaded', function() {
+    global $allow_updates;
+    if ($allow_updates) {
+        $embold_update_checker = PucFactory::buildUpdateChecker(
+            'https://github.com/emboldagency/embold-tailwind-blocks/',
+            __FILE__,
+            'embold-tailwind-blocks'
+        );
 
-// Set to use GitHub Releases
-$embold_update_checker->getVcsApi()->enableReleaseAssets();
+        // Set to use GitHub Releases
+        $embold_update_checker->getVcsApi()->enableReleaseAssets();
+    }
+}, 20);
 
 // Plugin initialization
 function embold_tailwind_blocks_init() {
